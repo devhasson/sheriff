e8c4b8a9714b88608c9c8d72edfb35e2d7c23887
Davi Hasson Castro <davihasson@gmail.com>
2025-03-29 17:58:36 -0300

    feat: enhance guild model and implement middleware for server usage restrictions in Discord bot

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -1,8 +1,31 @@
 import { setupCreators } from "#base";
+import { prisma } from "#database";
 
 export const { createCommand, createEvent, createResponder } = setupCreators({
   commands: {
-    guilds: ["1324180540416589864"],
+    guilds: (
+      await prisma.guild.findMany({
+        where: {
+          allowedGuild: {
+            not: null,
+          },
+        },
+      })
+    ).map((guild) => guild.id),
   },
-  responders: {},
+  responders: {
+    async middleware(interaction, _params, block) {
+      if (
+        interaction.guildId &&
+        !(await prisma.guild.findUnique({ where: { id: interaction.guildId } }))
+      ) {
+        await interaction.reply({
+          content: "This bot is not allowed to be used in this server.",
+          ephemeral: true,
+        });
+
+        return block();
+      }
+    },
+  },
 });
